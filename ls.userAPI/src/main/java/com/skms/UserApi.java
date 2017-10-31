596827a9a10ad4b02f04d9e27d63516fff608e8a
chenyaohua1990 <chenyaohua1990@foxmail.com>
2017-11-01 02:25:11 +0800

    修改报错

--- ls.userAPI/src/main/java/com/skms/UserApi.java
+++ ls.userAPI/src/main/java/com/skms/UserApi.java
@@ -1,16 +1,14 @@
 package com.skms;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
 import java.util.Map;
 @Path("userApi")
 @Produces(MediaType.APPLICATION_JSON)
 public interface UserApi {
     @GET
     @Path("login")
-    public String login(@QueryParam("")Map<String,String> map);
+    public String login(@QueryParam("")HashMap<String, String> map);
 
 }
--- ls.userImp/src/main/java/com/skms/UserImp.java
+++ ls.userImp/src/main/java/com/skms/UserImp.java
@@ -1,16 +1,18 @@
 package com.skms;
 
+import com.alibaba.fastjson.JSON;
 import com.skms.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-@Service("userImp")
+import java.util.HashMap;
+@Service
 public class UserImp implements UserApi {
     @Autowired
     private UserService userSerice;
 
-    public String login(Map<String, String> map) {
+    public String login(HashMap<String, String> map) {
+        System.out.print(JSON.toJSONString(map));
         userSerice.login(map);
         return null;
     }
